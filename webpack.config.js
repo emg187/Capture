cb3f5552ec0c49064abb5d78f68247a7d63431d7
Eric George <geric738@aol.com>
2019-06-08 20:37:06 -0700

    webpack setup to bundle js files and other assets

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public/dist'), 
+    publicPath: "dist/"
+  }, 
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+        'style-loader',
+        'css-loader'
+        ]
+      }, 
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          "file-loader?name=images/[name].[ext]"
+        ]
+      }
+    ]
+  }
+};
+
